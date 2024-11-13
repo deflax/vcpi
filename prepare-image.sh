fa1fc3f0b9c879bfc5b0df73b42915b31662c7fd
deflax <daniel@deflax.net>
2024-11-14 00:16:38 +0200

    setup cardinal with autorun and default patch

--- prepare-image.sh
+++ prepare-image.sh
@@ -30,15 +30,26 @@ chown 1000:1000 /tmp/rpi-img/home/pi/.ssh/authorized_keys
 #provision scripts
 cp -v ./src/setup.sh /tmp/rpi-img/root/setup.sh
 
+#enable systemd-time-wait-sync
+ln -v -s /lib/systemd/system/systemd-time-wait-sync.service /tmp/rpi-img/etc/systemd/system/sysinit.target.wants/systemd-time-wait-sync.service
+
 #setup autorun on first boot
 cp -v ./src/firstboot.service /tmp/rpi-img/lib/systemd/system/firstboot.service
 ln -v -s /lib/systemd/system/firstboot.service /tmp/rpi-img/etc/systemd/system/multi-user.target.wants
 
-#enable systemd-time-wait-sync
-ln -v -s /lib/systemd/system/systemd-time-wait-sync.service /tmp/rpi-img/etc/systemd/system/sysinit.target.wants/systemd-time-wait-sync.service
-
 # disable built-in audio
 sed -i 's/^dtparam=audio=on/#&/' /tmp/rpi-img/boot/config.txt
+
+# setup Cardinal
+mkdir -v /tmp/rpi-img/opt/Cardinal
+wget https://github.com/DISTRHO/Cardinal/releases/download/24.09/Cardinal-linux-aarch64-24.09.tar.gz -O /tmp/rpi-img/opt/Cardinal/Cardinal-linux-aarch64.tar.gz
+tar -xzvf /tmp/rpi-img/opt/Cardinal/Cardinal-linux-aarch64.tar.gz -C /tmp/rpi-img/opt/Cardinal/ CardinalNative
+
+# setup GUI payload
+ln -v -s /lib/systemd/system/payload.service /tmp/rpi-img/etc/systemd/system/graphical.target.wants
+
+# deploy native patch
+cp -v ./patch/native.vcv /home/pi/Documents/templates/native.vcv
 
 echo "] press enter to write the image"
 read
